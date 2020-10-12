f6ecec3236222247a3c52d31a149fd89ab23738c
VInicius-Mance <vinimanceprof@gmail.com>
2020-10-12 18:41:50 -0300

    PHP update
    correção de versão back end

--- PHP/calculo.php
+++ PHP/calculo.php
@@ -1,25 +1,66 @@
 <?php
-$cent5=0;$cent10=0;$cent25=0;$cent50=0;$real1=0;
-$real2=0;$real5=0;$real10=0;$real20=0;$real50=0;$real100=0;$real200=0;
-function calcular_dinheiro($cent5=0,$cent10=0,$cent25=0,$cent50=0,$real1=0,$real2=0,$real5=0,$real10=0,$real20=0,$real50=0,$real100=0,$real200=0){
-  if (empty($_POST["5_crs"]) and empty($_POST["10_crs"]) and empty($_POST["25_crs"]) and empty($_POST["50_crs"]) and empty($_POST["1_rs"]) and
-   empty($_POST["2_rs"]) and empty($_POST["5_rs"]) and empty($_POST["10_rs"]) and empty($_POST["20_rs"]) and empty($_POST["50_rs"])
-   and empty($_POST["100_rs"]) and empty($_POST["200_rs"])){
-      echo "Não há nada a ser calculado";
-      exit;
-    } else {
-      if(empty($_POST["5_crs"])){$_POST["5_crs"] = 0;} if(empty($_POST["10_crs"])){$_POST["10_crs"] = 0;}
-      if(empty($_POST["25_crs"])){$_POST["25_crs"] = 0;}  if(empty($_POST["50_crs"])){$_POST["50_crs"] = 0;}
-      if(empty($_POST["1_rs"])){$_POST["1_rs"] = 0;} if(empty($_POST["2_rs"])){$_POST["2_rs"] = 0;}
-      if(empty($_POST["5_rs"])){$_POST["5_rs"] = 0;} if(empty($_POST["10_rs"])){$_POST["10_rs"] = 0;}
-      if(empty($_POST["20_rs"])){$_POST["20_rs"] = 0;} if(empty($_POST["50_rs"])){$_POST["50_rs"] = 0;}
-      if(empty($_POST["100_rs"])){$_POST["100_rs"] = 0;} if(empty($_POST["200_rs"])){$_POST["200_rs"] = 0;}
-      $cent5 = $_POST["5_crs"]; $cent10 = $_POST["10_crs"]; $cent25 = $_POST["25_crs"]; $cent50 = $_POST["50_crs"];
-      $real1 = $_POST["1_rs"]; $real2 = $_POST["2_rs"]; $real5 = $_POST["5_rs"]; $real10 = $_POST["10_rs"];
-      $real20 = $_POST["20_rs"]; $real50 = $_POST["50_rs"]; $real100 = $_POST["100_rs"]; $real200 = $_POST["200_rs"];
-      $total = ($cent5 * 5 + $cent10 * 10 + $cent25 * 25 + $cent50 * 50 + $real1 * 100 + $real2 * 200
-      + $real5 * 500 + $real10 * 1000 + $real20 * 2000 + $real50 * 5000 + $real100 * 10000 + $real200 * 20000) / 100;
-      echo "Você tem no total R$: ". number_format($total, 2, ',', '.');
-    }
-  }
+  $cent5="";
+  $cent10="";
+  $cent25="";
+  $cent50="";
+  $real1="";
+  $real2="";
+  $real5="";
+  $real10="";
+  $real20="";
+  $real50="";
+  $real100="";
+  $real200="";
+
+   function calcular_dinheiro(
+    $cent5=0,$cent10=0,$cent25=0,
+    $cent50=0,$real1=0,$real2=0,
+    $real5=0,$real10=0,$real20=0,
+    $real50=0,$real100=0,$real200=0){
+       if(empty($_POST["5_crs"])){$_POST["5_crs"] = 0;}
+       if(empty($_POST["10_crs"])){$_POST["10_crs"] = 0;}
+       if(empty($_POST["25_crs"])){$_POST["25_crs"] = 0;}
+       if(empty($_POST["50_crs"])){$_POST["50_crs"] = 0;}
+       if(empty($_POST["1_rs"])){$_POST["1_rs"] = 0;}
+       if(empty($_POST["2_rs"])){$_POST["2_rs"] = 0;}
+       if(empty($_POST["5_rs"])){$_POST["5_rs"] = 0;}
+       if(empty($_POST["10_rs"])){$_POST["10_rs"] = 0;}
+       if(empty($_POST["20_rs"])){$_POST["20_rs"] = 0;}
+       if(empty($_POST["50_rs"])){$_POST["50_rs"] = 0;}
+       if(empty($_POST["100_rs"])){$_POST["100_rs"] = 0;}
+       if(empty($_POST["200_rs"])){$_POST["200_rs"] = 0;}
+
+       $cent5 = $_POST["5_crs"];
+       $cent10 = $_POST["10_crs"];
+       $cent25 = $_POST["25_crs"];
+       $cent50 = $_POST["50_crs"];
+       $real1 = $_POST["1_rs"];
+       $real2 = $_POST["2_rs"];
+       $real5 = $_POST["5_rs"];
+       $real10 = $_POST["10_rs"];
+       $real20 = $_POST["20_rs"];
+       $real50 = $_POST["50_rs"];
+       $real100 = $_POST["100_rs"];
+       $real200 = $_POST["200_rs"];
+
+       $total =
+       $cent5 * 0.05 +
+       $cent10 * 0.1 +
+       $cent25 * 0.25 +
+       $cent50 * 0.50 +
+       $real1 * 1 +
+       $real2 * 2+
+       $real5 * 5 +
+       $real10 * 10 +
+       $real20 * 20 +
+       $real50 * 50 +
+       $real100 * 100 +
+       $real200 * 200;
+
+        if ($total != 0) {
+         return "Você tem no total R$: ". number_format($total, 2, ',', '.');
+        } else {
+         return "Não há nada a calcular";
+        }
+   }
 ?>
--- PHP/index.php
+++ PHP/index.php
@@ -1,48 +1,48 @@
-<?php include('grana.php'); ?>
+<?php include('calculo.php'); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <link rel="stylesheet" href="css/grana.css">
+    <link rel="stylesheet" href="../css/grana.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de dinheiro</title>
   </head>
   <body>
-    <form class="container" action="granaform.php" method="post">
+    <form class="container" action="index.php" method="post">
       <div class="main">
         <label for="grana"><h1>Calcule seu dinheiro físico</h1></label><br>
       </div>
       <div class="moedas">
         <label for="5_crs">Moedas de 5 centavos (R$:0,05)</label><br>
-          <input class="dinheiro" type="number" name="5_crs" placeholder="0,05"><br>
+          <input class="dinheiro" type="number" name="5_crs" placeholder="0,05" value="<?php echo $cent5; ?>"><br>
         <label for="10_crs">Moedas de 10 centavos (R$:0,10)</label><br>
-          <input class="dinheiro" type="number" name="10_crs" placeholder="0,10"><br>
+          <input class="dinheiro" type="number" name="10_crs" placeholder="0,10" value="<?php echo $cent10; ?>"><br>
         <label for="25_crs">Moedas de 25 centavos (R$:0,25)</label><br>
-          <input class="dinheiro" type="number" name="25_crs" placeholder="0,25"><br>
+          <input class="dinheiro" type="number" name="25_crs" placeholder="0,25" value="<?php echo $cent25; ?>"><br>
         <label for="50_crs">Moedas de 50 centavos (R$:0,50)</label><br>
-          <input class="dinheiro" type="number" name="50_crs" placeholder="0,50"><br>
+          <input class="dinheiro" type="number" name="50_crs" placeholder="0,50" value="<?php echo $cent50; ?>"><br>
         <label for="1_rs">Moedas de 1 real (R$:1,00)</label><br>
-          <input class="dinheiro" type="number" name="1_rs" placeholder="1,00"><br>
+          <input class="dinheiro" type="number" name="1_rs" placeholder="1,00" value="<?php echo $real1; ?>"><br>
         <label for="2_rs">Notas de 2 reais (R$:2,00)</label><br>
-          <input class="dinheiro" type="number" name="2_rs" placeholder="2,00"><br>
+          <input class="dinheiro" type="number" name="2_rs" placeholder="2,00" value="<?php echo $real2; ?>"><br>
         </div>
         <div class="notas">
         <label for="5_rs">Notas de 5 reais (R$:5,00)</label><br>
-          <input class="dinheiro" type="number" name="5_rs" placeholder="5,00"><br>
+          <input class="dinheiro" type="number" name="5_rs" placeholder="5,00" value="<?php echo $real5; ?>"><br>
         <label for="10_rs">Notas de 10 reais (R$:10,00)</label><br>
-          <input class="dinheiro" type="number" name="10_rs" placeholder="10,00"><br>
+          <input class="dinheiro" type="number" name="10_rs" placeholder="10,00" value="<?php echo $real10; ?>"><br>
         <label for="20_rs">Notas de 20 reais (R$:20,00)</label><br>
-          <input class="dinheiro" type="number" name="20_rs"  placeholder="20,00"><br>
+          <input class="dinheiro" type="number" name="20_rs"  placeholder="20,00" value="<?php echo $real20; ?>"><br>
         <label for="50_rs">Notas de 50 reais (R$:50,00)</label><br>
-          <input class="dinheiro" type="number" name="50_rs" placeholder="50,00"><br>
+          <input class="dinheiro" type="number" name="50_rs" placeholder="50,00" value="<?php echo $real50; ?>"><br>
         <label for="100_rs">Notas de 100 reais (R$:100,00)</label><br>
-          <input class="dinheiro" type="number" name="100_rs" placeholder="100,00"><br>
+          <input class="dinheiro" type="number" name="100_rs" placeholder="100,00" value="<?php echo $real100; ?>"><br>
         <label for="200_rs">Notas de 200 reais (R$:200,00)</label><br>
-          <input class="dinheiro" type="number" name="200_rs" placeholder="200,00"><br>
+          <input class="dinheiro" type="number" name="200_rs" placeholder="200,00" value="<?php echo $real200; ?>"><br>
         </div>
         <div class="calcular">
           <button class="somar" type="submit" name="somar">Somar</button><br>
-        <p id="resultado"><?php calcular_dinheiro($cent5,$cent10,$cent25,$cent50,$real1,$real2,$real5,$real10,$real20,$real50,$real100,$real200);?></p>
+        <p id="resultado"><?php echo calcular_dinheiro($cent5,$cent10,$cent25,$cent50,$real1,$real2,$real5,$real10,$real20,$real50,$real100,$real200);?></p>
       </div>
     </form>
   </body>
